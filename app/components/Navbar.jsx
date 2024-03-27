3f736da16f7125ac76dea472a3c553dada96b011
Laplace <sadeeznut@gmail.com>
2024-03-27 15:46:58 +0800

    default

---
+++ app/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const Navbar = () => {
+  return (
+    <>
+      <nav className="flex justify-between mb-12 border-b border-violet-100 p-4">
+        <h1 className="font-bold text-2xl text-gray-700">Calendar</h1>
+      </nav>
+    </>
+  )
+}
+
+export default Navbar;
